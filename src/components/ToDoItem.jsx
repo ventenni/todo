78a6585fc6a28fcba0745752262bee65b94493f6
ventenni <ventenni@gmail.com>
2022-02-21 10:52:27 -0800

    Changed list item icon to info (i).

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -71,10 +71,11 @@ const ToDoItem = ({ item, toggleReminder, updateItemDate, updateItemTime }) => {
 								width="16"
 								height="16"
 								fill="currentColor"
-								className="bi bi-caret-down-fill"
+								class="bi bi-info-circle"
 								viewBox="0 0 16 16"
 							>
-								<path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+								<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+								<path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
 							</svg>
 						</button>
 					</Col>
